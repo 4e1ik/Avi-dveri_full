21cf1bd34b7055963d1a69eaeda7c3f221b829f0
artemi <Artemi324@tut.by>
2024-12-30 21:59:32 +0300

    init(27.12.2024): fix some bugs

--- resources/views/layouts/avi-dveri/avi-dveri.blade.php
+++ resources/views/layouts/avi-dveri/avi-dveri.blade.php
@@ -82,6 +82,7 @@
                                 <li><a class="header-menu" href="80293673518">+375 (29) 367-35-18</a></li>
                                 <li><a class="header-menu" href="80333943324">+375 (33) 394-33-24</a></li>
                                 <li>@livewire('search')</li>
+{{--                                @livewire('search')--}}
                             </ul>
                         </nav>
                     </div>
--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,24 +1,33 @@
-<form class="d-flex">
-    <div>
-        <input type="text" wire:model.live="searchTerm" style="padding: 10px; border: 1px solid #ccc; border-radius: 5px;"/>
+{{--<style>--}}
+{{--    div > p:hover {--}}
+{{--        background: #e6e6e6;--}}
+{{--    }--}}
+
+{{--    .d-flex > input {--}}
+
+{{--    }--}}
+
+
+
+{{--</style>--}}
+
+<form class="d-flex search_form">
+    <div class="search_form_group">
+        <input placeholder="ПОИСК" type="text" wire:model.live="searchTerm"
         @if(sizeof($results) >  0)
-        <div style="position: absolute; top: 80px; left: 65%; right: 10px; max-height: 300px; overflow-y: auto; z-index: 1000; background: #fff; border: 1px solid #ddd; border-radius: 10px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); padding: 10px; width: 25%; ">
+        <div class="search_example" style="">
             @foreach($results as $result)
                 <a href="{{route('product_page', ['id' => $result->id, 'class' => get_class($result)])}}">
-                    <p style="margin: 5px 0; padding: 10px; background: #f9f9f9; border-radius: 5px; transition: background 0.3s ease; cursor: pointer; ">
+                    <p class="search_text">
                         {{$result->title}}
                     </p>
                 </a>
             @endforeach
         </div>
         @endif
-        <style>
-            div > p:hover {
-                background: #e6e6e6;
-            }
-        </style>
+
     </div>
-    <a class="search-img" href="#">
-        <img src="{{asset('/avi-dveri_assets/avi-dveri/img/search.png')}}" alt="" width="30" height="30" class="d-inline-block align-top" alt="CoreUI Logo">
-    </a>
+{{--    <a class="search-img" href="#">--}}
+{{--        <img src="{{asset('/avi-dveri_assets/avi-dveri/img/search.png')}}" alt="" width="30" height="30" class="align-top" alt="CoreUI Logo">--}}
+{{--    </a>--}}
 </form>
