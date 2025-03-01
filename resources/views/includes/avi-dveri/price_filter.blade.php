149a84e1300366a52e31b156caa4962fca2f6005
artemi <Artemi324@tut.by>
2025-03-02 00:11:13 +0300

    init(01.03.2025): fix styles 9

--- resources/views/includes/avi-dveri/price_filter.blade.php
+++ resources/views/includes/avi-dveri/price_filter.blade.php
@@ -1,19 +1,19 @@
 <aside class="widget shop-filter mb-30">
     <div class="widget-title">
         <h4>Цена</h4>
-            <div class="price__label">
-                <form action="" method="post">
-                    @csrf
-                        <label for="button1">
-                            <input name="price_filter" value="DESC" type="radio" id="button1">
-                            <button type="submit">↑</button>
-                        </label>
-                        <label for="button2">
-                            <input name="price_filter" value="ASC" type="radio" id="button2">
-                            <button type="submit">↓</button>
-                        </label>
-                </form>
-            </div>
+        <div class="price__label">
+            <form action="" method="post">
+                @csrf
+                <button type="submit">
+                    <input name="price_filter" value="DESC" type="radio" id="button1">
+                    <label for="button1">↑</label>
+                </button>
+                <button type="submit">
+                    <input name="price_filter" value="ASC" type="radio" id="button2">
+                    <label for="button2">↓</label>
+                </button>
+            </form>
+        </div>
     </div>
     {{--<div class="widget-info">--}}
     {{--    <div class="price_filter">--}}
