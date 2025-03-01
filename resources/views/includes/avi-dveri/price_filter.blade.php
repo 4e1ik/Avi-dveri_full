ca8478a72a1558d7b4ee402cd7e1b8945f3a43d3
artemi <Artemi324@tut.by>
2025-03-02 00:00:20 +0300

    init(01.03.2025): fix styles 4

--- resources/views/includes/avi-dveri/price_filter.blade.php
+++ resources/views/includes/avi-dveri/price_filter.blade.php
@@ -1,20 +1,23 @@
 <aside class="widget shop-filter mb-30">
     <div class="widget-title">
         <h4>Цена</h4>
-        <div style="-webkit-appearance: none;" class="price__label">
-            <form action="" method="post">
-                @csrf
-                <button type="submit">
-                    <input name="price_filter" value="DESC" type="radio" id="button1">
-                    <label for="button1">↑</label>
-                </button>
-                <button type="submit">
-                    <input name="price_filter" value="ASC" type="radio" id="button2">
-                    <label for="button2">↓</label>
-                </button>
-            </form>
-
-        </div>
+            <div class="price__label">
+                <form action="" method="post">
+                    @csrf
+                    <button type="submit">
+                        <input name="price_filter" value="DESC" type="radio" id="button1">
+                        <label style="-webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;" for="button1">↑</label>
+                    </button>
+                    <button type="submit">
+                        <input name="price_filter" value="ASC" type="radio" id="button2">
+                        <label style="-webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;" for="button2">↓</label>
+                    </button>
+                </form>
+            </div>
     </div>
     {{--<div class="widget-info">--}}
     {{--    <div class="price_filter">--}}
