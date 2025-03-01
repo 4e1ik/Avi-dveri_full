4ab5a7a3682b5d110ec5223ae1283058186385f2
artemi <Artemi324@tut.by>
2025-03-02 00:39:12 +0300

    init(01.03.2025): fix fittings aside

--- resources/views/avi-dveri/avi-dveri/accessories.blade.php
+++ resources/views/avi-dveri/avi-dveri/accessories.blade.php
@@ -64,26 +64,7 @@
                     <!-- Widget-categories end -->
                     <!-- Shop-Filter start -->
                     <form action="">
-                        <aside class="widget shop-filter mb-30">
-                            <div class="widget-title">
-                                <h4>Цена</h4>
-                                <div class="price__label">
-                                    <input name="price_filter" value="ASC" type="radio" id="button1">
-                                    <label for="button1">↑</label>
-                                    <input name="price_filter" value="DESC" type="radio" id="button2">
-                                    <label for="button2">↓</label>
-                                </div>
-                            </div>
-                            <div class="widget-info">
-                                <div class="price_filter">
-                                    <div class="price_slider_amount">
-                                        <input type="submit" value="Диапазон :"/>
-                                        <input type="text" id="amount" name="price" placeholder="Add Your Price"/>
-                                    </div>
-                                    <div id="slider-range"></div>
-                                </div>
-                            </div>
-                        </aside>
+                        @include('includes.avi-dveri.price_filter')
                         <!-- Shop-Filter end -->
                         <!-- Widget-Manufacturer start -->
                         <aside class="widget widget-color mb-30">
@@ -105,13 +86,13 @@
                             </div>
                         </aside>
 
-                        <button style="width: 100%; height: 45px; font-size: larger; line-height: 45px;"
-                                data-text="Отфильтровать" type="submit" class="button-one submit-btn-4">Отфильтровать
-                        </button>
+{{--                        <button style="width: 100%; height: 45px; font-size: larger; line-height: 45px;"--}}
+{{--                                data-text="Отфильтровать" type="submit" class="button-one submit-btn-4">Отфильтровать--}}
+{{--                        </button>--}}
                     </form>
 
                     <!-- Widget-Manufacturer end -->
-                    <a href="{{route('accessories')}}">Очистить фильтр</a>
+{{--                    <a href="{{route('accessories')}}">Очистить фильтр</a>--}}
                 </div>
             </div>
         </div>
