95bcc27471b52d619d8467774bb941a892ca38ff
artemi <Artemi324@tut.by>
2025-03-20 16:47:06 +0300

    init(20.03.2025): edit solid doors name 2

--- resources/views/avi-dveri/avi-dveri/doors/interior_doors/solid_doors.blade.php
+++ resources/views/avi-dveri/avi-dveri/doors/interior_doors/solid_doors.blade.php
@@ -8,7 +8,7 @@
                 <div class="col-md-12">
                     <div class="heading-banner">
                         <div class="heading-banner-title">
-                            <h2>Скрытые</h2>
+                            <h2>Массив</h2>
                         </div>
                         <div class="breadcumbs pb-15">
                             <ul>
