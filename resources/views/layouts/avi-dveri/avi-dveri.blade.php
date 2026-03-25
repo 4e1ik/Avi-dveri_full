4c7826a393a4a29211c4d63445daf1f3f64b6038
Artemi Sevostian <asevostan@gmail.com>
2026-03-25 09:45:23 +0300

    feat: add g-tag for Google Analytics

--- resources/views/layouts/avi-dveri/avi-dveri.blade.php
+++ resources/views/layouts/avi-dveri/avi-dveri.blade.php
@@ -1,9 +1,16 @@
 <!doctype html>
 <html class="no-js" lang="en">
 <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TCM9F2TW');</script>
+    <!-- End Google Tag Manager -->
+
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-{{--    <meta name="csrf-token" content="{{ csrf_token() }}">--}}
     <title>{{ $metaTitle ?? 'АВИ-двери' }}</title>
     <meta name="description" content="{{ $metaDescription ?? '' }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -46,6 +53,10 @@
     @yield('404')
 </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TCM9F2TW"
+                  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <!-- WRAPPER START -->
 <div class="wrapper bg-dark-white">
     <header class="header">
